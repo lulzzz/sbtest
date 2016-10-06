d3bfa2f4de3bf78c1e0ca74c812ca8483fc37d08
Sherjeel <sherjeel.bedaar@gmail.com>
2016-10-06 18:41:01 +0500

    fixed commit issue

--- HrMaxxWeb/Scripts/app/common/common.js
+++ HrMaxxWeb/Scripts/app/common/common.js
@@ -5,7 +5,7 @@ common.constant('zionPaths', {
 	Logout: 'Account/LogOff',
 	Token: 'token'
 });
-common.constant('version', '1.0.0.8');
+common.constant('version', '1.0.0.9');
 common.constant('EntityTypes', {
 	Host:1,
 	Company:2,
