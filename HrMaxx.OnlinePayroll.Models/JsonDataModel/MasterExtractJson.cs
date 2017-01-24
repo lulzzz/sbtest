0c35003ad0281d42e0769c3fdf69f633b7b6d8a6
Sherjeel <sherjeel.bedaar@gmail.com>
2017-01-23 18:42:16 -0800

    missing file

---
+++ HrMaxx.OnlinePayroll.Models/JsonDataModel/MasterExtractJson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HrMaxx.OnlinePayroll.Models.JsonDataModel
+{
+	[Serializable]
+	[XmlRoot("MasterExtractList")]
+	public class MasterExtractJson
+	{
+		public int Id { get; set; }
+    public string Extract { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string ExtractName { get; set; }
+    public bool IsFederal { get; set; }
+    public DateTime DepositDate { get; set; }
+    public string Journals { get; set; }
+    public DateTime LastModified { get; set; }
+    public string LastModifiedBy { get; set; }
+    
+	}
+}
