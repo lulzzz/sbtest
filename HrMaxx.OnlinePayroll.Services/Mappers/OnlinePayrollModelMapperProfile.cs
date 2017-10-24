37637dca86fb065253df8cb2b30c36b19d6d3dad
Sherjeel <sherjeel.bedaar@gmail.com>
2017-10-24 21:55:16 +0500

    fixed mapper

--- HrMaxx.OnlinePayroll.Services/Mappers/OnlinePayrollModelMapperProfile.cs
+++ HrMaxx.OnlinePayroll.Services/Mappers/OnlinePayrollModelMapperProfile.cs
@@ -631,7 +631,8 @@ namespace HrMaxx.OnlinePayroll.Services.Mappers
 				.ForMember(dest => dest.UserName, opt => opt.Ignore())
 				.ForMember(dest => dest.Notes, opt => opt.Ignore())
 				.ForMember(dest => dest.PayrollMessage, opt => opt.Ignore())
-				.ForMember(dest => dest.Contact, opt => opt.Ignore());
+				.ForMember(dest => dest.Contact, opt => opt.Ignore())
+				.ForMember(dest => dest.IsFiler1095, opt => opt.Ignore());
 
 			CreateMap<Models.ExtractDBCompany, Common.Models.InsuranceGroupDto>()
 				.ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.InsuranceGroupNo))
